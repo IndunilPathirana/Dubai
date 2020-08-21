604aaa1b6e55fcdb05b32e8cd5bae05f4fec23c9
IndunilPathirana <66858806+IndunilPathirana@users.noreply.github.com>
2020-08-21 09:06:21 +0530

    Add files via upload

--- onpageload.js
+++ onpageload.js
@@ -8,26 +8,7 @@ $(document).ready(function () {
   });
 
   $(".lang").hide();
-  $("#sinhalaemb").click(function () {
-    // $("#embnm").html("<h1>ශ්‍රි ලංකා තනාපති කාර්යලය</h1>");
-    $("#title").html(' <h2 class="title">ලියාපදිංචි තොරතුරු</h2>');
 
-    $("#successMessage").val("ඔබ සාර්තකව ලියාපදින්චි වී ඇත");
-  });
-  $("#englishemb").click(function () {
-    // $("#embnm").html("<h1>Embassy of Sri Lanka</h1>");
-    $("#title").html(' <h2 class="title"> Registration Info</h2>');
-
-    $("#successMessage").val("Successfully Registerd and we are working on it");
-  });
-  $("#tamilemb").click(function () {
-    //     $("#embnm").html("<h1> இலங்கை தூதரகம் </h1>");
-    $("#title").html(' <h2 class="title"> பதிவு தகவல்</h2>');
-
-    $("#successMessage").val(
-      "வெற்றிகரமாக பதிவுசெய்துள்ளோம், நாங்கள் அதைச் செய்கிறோம்"
-    );
-  });
   //   alert("Hello");
 });
 
@@ -41,29 +22,107 @@ var firstName = "Indunil";
 var registerdDate;
 function getUserData() {
   var URL = rootURL.concat(passportNumber);
-  $.get(URL, function (data, status) {
-    $(".lang").fadeIn("slow");
 
-    if (data.form_id) {
-      // assign variables
-      var regDate = data.form_date;
-      var firstName = data.your_name;
-      var lastName = data.your_surname;
+  $.ajax({
+    url: URL,
+    error: function () {
+      // will fire when timeout is reached
+      alert("Time Out");
+    },
+    success: function (data, status) {
+      //do something
+      $(".lang").fadeIn("slow");
 
-      // assign values to html
-      $("#fName").val(firstName);
-      $("#lName").val(lastName);
-      $("#regDate").val(regDate);
-      $("#successMessage").val(
-        "Successfully Registerd and we are working on it"
-      );
-    } else {
-      $("#fName").val("No First Name Data");
-      $("#lName").val("No Last Name Data");
-      $("#regDate").val("No Reg Date Data");
-      $("#successMessage").val(
-        "We don't have data about you. You may have not registerd"
-      );
-    }
+      if (data.form_id) {
+        // assign variables
+        var regDate = data.form_date;
+        var firstName = data.your_name;
+        var lastName = data.your_surname;
+
+        // assign values to html
+        $("#fName").val(firstName);
+        $("#lName").val(lastName);
+        $("#regDate").val(regDate);
+        $("#successMessage").val(
+          "Successfully Registerd and we are working on it"
+        );
+
+        $("#sinhalaemb").click(function () {
+          // $("#embnm").html("<h1>ශ්‍රි ලංකා තනාපති කාර්යලය</h1>");
+          $("#title").html(' <h2 class="title">ලියාපදිංචි තොරතුරු</h2>');
+
+          $("#successMessage").val("ඔබ සාර්තකව ලියාපදින්චි වී ඇත");
+        });
+        $("#englishemb").click(function () {
+          // $("#embnm").html("<h1>Embassy of Sri Lanka</h1>");
+          $("#title").html(' <h2 class="title"> Registration Info</h2>');
+
+          $("#successMessage").val(
+            "Successfully Registerd and we are working on it"
+          );
+        });
+        $("#tamilemb").click(function () {
+          //     $("#embnm").html("<h1> இலங்கை தூதரகம் </h1>");
+          $("#title").html(' <h2 class="title"> பதிவு தகவல்</h2>');
+
+          $("#successMessage").val(
+            "வெற்றிகரமாக பதிவுசெய்துள்ளோம், நாங்கள் அதைச் செய்கிறோம்"
+          );
+        });
+      } else {
+        $("#fName").val("No First Name Data");
+        $("#lName").val("No Last Name Data");
+        $("#regDate").val("No Reg Date Data");
+        $("#successMessage").val(
+          "We don't have data about you. You may have not registerd"
+        );
+
+        $("#sinhalaemb").click(function () {
+          // $("#embnm").html("<h1>ශ්‍රි ලංකා තනාපති කාර්යලය</h1>");
+          $("#title").html(' <h2 class="title">ලියාපදිංචි තොරතුරු</h2>');
+
+          $("#successMessage").val("කනගාටුයි අප සතුව ඔබගේ තොරතුරු නොමැත");
+        });
+        $("#englishemb").click(function () {
+          // $("#embnm").html("<h1>Embassy of Sri Lanka</h1>");
+          $("#title").html(' <h2 class="title"> Registration Info</h2>');
+
+          $("#successMessage").val("Sorry We don't have data about you");
+        });
+        $("#tamilemb").click(function () {
+          //     $("#embnm").html("<h1> இலங்கை தூதரகம் </h1>");
+          $("#title").html(' <h2 class="title"> பதிவு தகவல்</h2>');
+
+          $("#successMessage").val(
+            "மன்னிக்கவும், உங்களைப் பற்றிய தரவு எங்களிடம் இல்லை"
+          );
+        });
+      }
+    },
+    timeout: 3000, // sets timeout to 3 seconds
   });
 }
+
+// $.get(URL, (timeout = 3000), function (data, status) {
+//   $(".lang").fadeIn("slow");
+
+//   if (data.form_id) {
+//     // assign variables
+//     var regDate = data.form_date;
+//     var firstName = data.your_name;
+//     var lastName = data.your_surname;
+
+//     // assign values to html
+//     $("#fName").val(firstName);
+//     $("#lName").val(lastName);
+//     $("#regDate").val(regDate);
+//     $("#successMessage").val("Successfully Registerd and we are working on it");
+//   } else {
+//     $("#fName").val("No First Name Data");
+//     $("#lName").val("No Last Name Data");
+//     $("#regDate").val("No Reg Date Data");
+//     $("#successMessage").val(
+//       "We don't have data about you. You may have not registerd"
+//     );
+//   }
+// });
